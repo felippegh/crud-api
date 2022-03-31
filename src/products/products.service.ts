99aefd5ae22d708455b663e840b090de07b0f62f
Felippe <felippegeorge@utexas.edu>
2022-03-31 20:16:07 -0300

    Add crud methods

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { Product } from './product.model';
 
@@ -6,12 +6,52 @@ const { v4: uuidv4 } = require('uuid');
 
 @Injectable()
 export class ProductService {
-    products: Product[] = [];
+    private products: Product[] = [];
 
     insertProduct(title: string, desc: string, price: number): string {
         const prodId = uuidv4();
         const newProduct = new Product(prodId, title, desc, price);
         this.products.push(newProduct);
         return prodId;
+    }
+
+    getProducts() {
+        return [...this.products];
+    }
+
+    getProduct(productId: string) {
+        const product = this.findProduct(productId)[0];
+        return { ...product };  
+    }
+    
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+        const [product, index] = this.findProduct(productId);
+        const updatedProduct = {...product};
+
+        if (title) {
+            updatedProduct.title = title;
+        }
+        if (desc) {
+            updatedProduct. description = desc;
+        }
+        if (price) {
+            updatedProduct.price = price;
+        }
+        this.products[index] = updatedProduct;  
+    }
+
+    deleteProduct(prodId: string) {
+        const [product, index] = this.findProduct(prodId);
+        this.products.splice(index, 1);
+    }
+
+    private findProduct(id: string): [Product, number] {
+        const productIndex = this.products.findIndex((prod) => prod.id === id);
+        const product = this.products[productIndex];
+
+        if (!product) {
+            throw new NotFoundException('Could not find product.');
+        }
+        return [product, productIndex];
     }
 }
